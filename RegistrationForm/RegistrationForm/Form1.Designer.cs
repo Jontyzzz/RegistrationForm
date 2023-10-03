b77bbeb6a8a79c11786052e5352e8f66f1d3f733
shrivikram <117815324+shrivikram@users.noreply.github.com>
2023-10-03 10:43:26 +0530

    Add files via upload

---
+++ RegistrationForm/RegistrationForm/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace RegistrationForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Btnclick_register = new System.Windows.Forms.Button();
+            this.Registration = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(316, 180);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(316, 234);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "password";
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(435, 180);
+            this.txtusername.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(132, 21);
+            this.txtusername.TabIndex = 2;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(435, 234);
+            this.txtpassword.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(132, 21);
+            this.txtpassword.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(401, 300);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 27);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Btnclick_register
+            // 
+            this.Btnclick_register.Location = new System.Drawing.Point(401, 367);
+            this.Btnclick_register.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Btnclick_register.Name = "Btnclick_register";
+            this.Btnclick_register.Size = new System.Drawing.Size(100, 27);
+            this.Btnclick_register.TabIndex = 5;
+            this.Btnclick_register.Text = "Create";
+            this.Btnclick_register.UseVisualStyleBackColor = true;
+            this.Btnclick_register.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Registration
+            // 
+            this.Registration.AutoSize = true;
+            this.Registration.Location = new System.Drawing.Point(431, 111);
+            this.Registration.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Registration.Name = "Registration";
+            this.Registration.Size = new System.Drawing.Size(47, 15);
+            this.Registration.TabIndex = 6;
+            this.Registration.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 519);
+            this.Controls.Add(this.Registration);
+            this.Controls.Add(this.Btnclick_register);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Btnclick_register;
+        private System.Windows.Forms.Label Registration;
+    }
+}
